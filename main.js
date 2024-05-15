722d9424156966761aa76b8b21ceb689fa6e50d9
mac <mac@MacBook-Pro-mac.local>
2024-05-15 20:40:50 +0300

    added 4_4

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-let placeOfResidence = prompt('Какой город', 'Киев');
-let DateOfBirth = prompt('Какой год рождения', '1992');
-let hobbies = prompt('твой любимый вид спорта?', 'футбол');
-
-if (DateOfBirth !== null && placeOfResidence !== null && hobbies !== null) {
-    alert(`${DateOfBirth},${placeOfResidence},${hobbies}`);
-} else {
-    console.log('нет данных');
+switch (numOrStr) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStr.trim() === ''):
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStr)):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
 }
