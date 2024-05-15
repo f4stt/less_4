c8ee77d02a668ea34b6f0d400e377c15fbdd312a
mac <mac@MacBook-Pro-mac.local>
2024-05-15 20:30:04 +0300

    added 4.1

---
+++ main.js
@@ -0,0 +1,7 @@
+// let userName = prompt('what is your name?');
+// let isName = 'John';
+// if (userName === isName) {
+//     alert('Hello, John! How are you?')
+// } else {
+//     alert('you entered the wrong name')
+// };
